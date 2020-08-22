c109c5dc44184d3c3754af217e95e62719f05309
wesley <zerofire.li@qq.com>
2020-08-22 12:14:57 +0800

    Initial commit

---
+++ src/test/java/com/blog/blogcloud/BlogCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.blogcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
